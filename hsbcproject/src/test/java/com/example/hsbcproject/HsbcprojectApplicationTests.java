e170f581bac5419f95fec97cf62f9118266d1aa6
Raj Bagri <rajbagri3214@gmail.com>
2026-08-02 08:17:19 +0000

    first commit

---
+++ hsbcproject/src/test/java/com/example/hsbcproject/HsbcprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hsbcproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HsbcprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
